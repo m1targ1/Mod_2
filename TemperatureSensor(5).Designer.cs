1c01e1d9a2d83ea50d48cdf1ef79c1371da6a9d6
m1targ1 <sdm.m1targ1@gmail.com>
2024-09-27 22:21:05 +0300

    TemperatureSensor(5)

--- TemperatureSensor(5).Designer.cs
+++ TemperatureSensor(5).Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button_Check = new Button();
+            textBox_Check = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox_info = new TextBox();
             SuspendLayout();
+            // 
+            // button_Check
+            // 
+            button_Check.Location = new Point(12, 56);
+            button_Check.Name = "button_Check";
+            button_Check.Size = new Size(239, 23);
+            button_Check.TabIndex = 0;
+            button_Check.Text = "Узнать";
+            button_Check.UseVisualStyleBackColor = true;
+            button_Check.Click += button_Check_Click;
+            // 
+            // textBox_Check
+            // 
+            textBox_Check.Location = new Point(12, 27);
+            textBox_Check.Name = "textBox_Check";
+            textBox_Check.Size = new Size(239, 23);
+            textBox_Check.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Введите температуру";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(14, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Инфо:";
+            // 
+            // textBox_info
+            // 
+            textBox_info.Enabled = false;
+            textBox_info.Location = new Point(12, 121);
+            textBox_info.Name = "textBox_info";
+            textBox_info.Size = new Size(239, 23);
+            textBox_info.TabIndex = 4;
             // 
             // TemperatureSensor_5_
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(267, 167);
+            Controls.Add(textBox_info);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox_Check);
+            Controls.Add(button_Check);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Name = "TemperatureSensor_5_";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "TemperatureSensor(5)";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button_Check;
+        private TextBox textBox_Check;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox_info;
     }
 }
--- TemperatureSensor(5).cs
+++ TemperatureSensor(5).cs
@@ -1,20 +1,114 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Mod_2
 {
     public partial class TemperatureSensor_5_ : Form
     {
+        // Экземпляры классов датчика температуры и термостата
+        private TemperatureSensor temperatureSensor;
+        private Thermostat thermostat;
+
         public TemperatureSensor_5_()
         {
             InitializeComponent();
+
+            // Инициализация "датчика" температуры и термостата
+            temperatureSensor = new TemperatureSensor();
+            thermostat = new Thermostat();
+
+            // Подписка термостата на событие изменения температуры
+            temperatureSensor.TemperatureChanged += (sender, e) =>
+            {
+                    // Вызов метода термостата при изменении температуры
+                    thermostat.OnTemperatureChanged(sender, e, textBox_info);
+            };
+        }
+
+        private void button_Check_Click(object sender, EventArgs e)
+        {
+            textBox_info.Text = ""; // Очистка поля для вывода информации
+            // Проверка на то, что поле ввода температуры не пустое
+            if (!string.IsNullOrEmpty(textBox_Check.Text))
+            {
+                // Попытка преобразовать введенное значение в тип float
+                if (float.TryParse(textBox_Check.Text, out float temperature))
+                {
+                    // Если преобразование успешно, присвоить значение текущей температуре датчика
+                    temperatureSensor.CurrentTemperature = temperature;
+                }
+            }
+            // Если поле пустое, вывод сообщения об ошибке
+            else
+            {
+                MessageBox.Show("Поле температуры не может быть пустым.");
+            }
+        }
+    }
+    // Делегат для события изменения температуры
+    public delegate void TemperatureChangedHandler(object sender, TemperatureChangedEventArgs e);
+
+    // Класс для хранения данных о температуре
+    public class TemperatureChangedEventArgs : EventArgs
+    {
+        // Свойство для хранения нового значения температуры
+        public float NewTemperature { get; }
+
+        // Конструктор класса TemperatureChangedEventArgs
+        public TemperatureChangedEventArgs(float newTemperature)
+        {
+            NewTemperature = newTemperature;
+        }
+    }
+    // Класс датчика температуры
+    public class TemperatureSensor
+    {
+        private float _currentTemperature; // Поле для хранения текущей температуры
+        public event TemperatureChangedHandler TemperatureChanged = delegate { };
+
+        public float CurrentTemperature // Свойство для изменения и получения текущей температуры
+        {
+            get => _currentTemperature;
+            set
+            {
+                // Если значение температуры изменилось
+                if (_currentTemperature != value)
+                {
+                    _currentTemperature = value;
+                    OnTemperatureChanged(new TemperatureChangedEventArgs(_currentTemperature)); // Вызов события изменения температуры
+                }
+            }
+        }
+        // Метод для вызова события TemperatureChanged
+        protected virtual void OnTemperatureChanged(TemperatureChangedEventArgs e)
+        {
+            TemperatureChanged?.Invoke(this, e); // Если на событие подписаны обработчики, оно вызывается
+        }
+    }
+    // Класс термостата
+    public class Thermostat
+    {        
+        private const float HeatingThreshold = 20.0f; // Пороговое значение для включения отопления
+
+        // Метод обработки изменения температуры
+        public void OnTemperatureChanged(object sender, TemperatureChangedEventArgs e, TextBox textBox_info)
+        {
+            // Если температура ниже порогового значения, включить отопление
+            if (e.NewTemperature < HeatingThreshold)
+            {
+                string info = "Отопление включено.";
+                textBox_info.Text = info;
+            }
+            // Если температура выше порогового значения, выключить отопление
+            else
+            {
+                string info = "Отопление выключено.";
+                textBox_info.Text = info;
+            }
         }
     }
 }
